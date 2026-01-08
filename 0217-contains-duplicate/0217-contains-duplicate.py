b14c76aaf5f39485607802a78d3daf5b622057f1
balawal chaudry <62568625+Balawal@users.noreply.github.com>
2026-01-08 13:09:49 -0500

    Time: 11 ms (73.09%), Space: 32.3 MB (21.14%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,12 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
 
-        seen = set()
+        seen = set()    #use a set to track seen numbers
 
-        for num in nums:
-            if num in seen:
+        for num in nums:        #check each number
+            if num in seen:     #if we've seen it before, return true
                 return True
             
-            seen.add(num)
+            seen.add(num)       #else, add it to the set
         
-        return False
+        return False            #return false if no duplicate found
